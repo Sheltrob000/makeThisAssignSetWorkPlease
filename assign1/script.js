b266051e814a128b99b49535b34ccba2aa0f0cc0
sheltrob000 <sheltrob000@warren.k12.in.us>
2025-01-15 12:14:55 -0500

    init commit

--- assign1/script.js
+++ assign1/script.js
@@ -3,7 +3,7 @@ const book = {
     author: "authorName",
     year: "some year I don't know like 1895.3 or somthing",
     getSummary: function (){
-        return "this is a book with words, aranged in a way that makes sence to people understanding the english lexicon."
+        return `${this.title} ${this.author} ${this.year}`
     }
 
 }
