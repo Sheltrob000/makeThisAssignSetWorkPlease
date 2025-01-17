aaa7943bc46720b85b1a309aa8661b9d76a854df
sheltrob000 <sheltrob000@warren.k12.in.us>
2025-01-17 14:02:11 -0500

    dom manipulation pretty

--- assign2domManuplation/script.js
+++ assign2domManuplation/script.js
@@ -11,3 +11,35 @@ subButton.addEventListener("click", () => {
     count -= 1
     counter.innerHTML = count;})
 
+
+
+
+let interval;
+
+function startCounterUp(){
+    if (!interval){
+        interval = setInterval(countUp, 10);
+    }
+}
+function countUp(){
+    count += 1;
+    counter.innerHTML = count;
+}
+
+function countDown(){
+    count -= 1;
+    counter.innerHTML = count
+}
+
+function endCounterUp(){
+    clearInterval(interval)
+    interval = null;
+}
+
+
+plusButton.addEventListener("mouseover", startCounterUp);
+plusButton.addEventListener("mouseout", endCounterUp);
+
+subButton.addEventListener("mouseover", startCounterUp);
+subButton.addEventListener("mouseout", endCounterUp)
+
